f7b2a96a6d728200a63539e24fae7afe4ef9480d
Phillip H. Blanton <phillip@radwarrior.com>
2020-10-09 13:02:56 -0600

    Updated the code file headers.

--- Source/TestService/Properties/AssemblyInfo.cs
+++ Source/TestService/Properties/AssemblyInfo.cs
@@ -5,7 +5,6 @@
 * 
 * Copyright © 2006-2020 Phillip H. Blanton / Gort Technology (http://gort.co) All rights reserved.
 *
-* Copyright (C) ${Year} [name of author]
 * This program is free software; you can redistribute it and/or modify it under the terms of the 
 * GNU General Public License as published by the Free Software Foundation; either version 2 of 
 * the License, or (at your option) any later version.
